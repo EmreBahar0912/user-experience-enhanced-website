4aa5b04b4671e0f6d6db974880d4258fbb96964b
EmreBahar0912 <e.bahar906@gmail.com>
2026-05-06 14:51:40 +0200

    Comments verwijderd server.js #10
    Comments verwijderd van server.js voor een betere performance.

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
 import express from 'express'
 
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
 
 console.log('Hieronder moet je waarschijnlijk nog wat veranderen')
-// Doe een fetch naar de data die je nodig hebt
-// const apiResponse = await fetch('...')
-
-// Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-// const apiResponseJSON = await apiResponse.json()
-
-// Controleer eventueel de data in je console
-// (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
-// console.log(apiResponseJSON)
 
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
-// Maak werken met data uit formulieren iets prettiger
 app.use(express.urlencoded({extended: true}))
 
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
-// Stel Liquid in als 'view engine'
 const engine = new Liquid();
 app.engine('liquid', engine.express()); 
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
-// Maak een GET route voor de index (meestal doe je dit in de root, als /)
 app.get('/', async function (request, response) {
   const districts = ['algemeen', 'nieuw-west', 'oost', 'zuidoost']
 
@@ -73,8 +53,7 @@ app.get('/', async function (request, response) {
 })
 
 app.get('/nieuw-west', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'nieuw-west',
     'filter[date][_gte]': '2025-01-01',
@@ -83,17 +62,15 @@ app.get('/nieuw-west', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('nieuw-west.liquid', {stories: apiResponseJSON.data, page: 'nieuwwest'})
 })
 
 app.get('/oost', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'oost',
     'filter[date][_gte]': '2025-01-01',
@@ -102,17 +79,15 @@ app.get('/oost', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('oost.liquid', {stories: apiResponseJSON.data, page: 'oost'})
 })
 
 app.get('/zuidoost', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'zuidoost',
     'filter[date][_gte]': '2025-01-01',
@@ -121,11 +96,10 @@ app.get('/zuidoost', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('zuidoost.liquid', {stories: apiResponseJSON.data, page: 'zuidoost'})
 })
 
@@ -138,11 +112,10 @@ app.get('/zoeken', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('index.liquid', {stories: apiResponseJSON.data, search})
 })
 
@@ -155,17 +128,15 @@ app.get('/zoeken', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('nieuw-west', {stories: apiResponseJSON.data, search})
 })
 
 app.get('/algemeen-nieuws-nieuw-oud', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'algemeen',
     'fields': 'title, intro, date, cover.*',
@@ -173,17 +144,15 @@ app.get('/algemeen-nieuws-nieuw-oud', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
 app.get('/algemeen-nieuws-oud-nieuw', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'algemeen',
     'fields': 'title, intro, date, cover.*',
@@ -191,17 +160,15 @@ app.get('/algemeen-nieuws-oud-nieuw', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
 app.get('/algemeen-doelgroep-wijkpartner', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district]': 'algemeen',
     'filter[target_group]': 'wijkpartner',
@@ -210,17 +177,15 @@ app.get('/algemeen-doelgroep-wijkpartner', async function (request, response) {
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(personResponseJSON.data)
+
    response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
 app.get('/archief-2023', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district][_in]': 'algemeen, nieuw-west, oost, zuidoost',
     'filter[date][_gte]': '2023-01-01',
@@ -233,13 +198,12 @@ app.get('/archief-2023', async function (request, response) {
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(apiResponseJSON) // wat geeft dit terug?
-   response.render('archief2023.liquid', {stories: apiResponseJSON.data})
+
+  response.render('archief2023.liquid', {stories: apiResponseJSON.data})
 })
 
 app.get('/archief-2024', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district][_in]': 'algemeen, nieuw-west, oost, zuidoost',
     'filter[date][_gte]': '2024-01-01',
@@ -252,13 +216,12 @@ app.get('/archief-2024', async function (request, response) {
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(apiResponseJSON) // wat geeft dit terug?
+
    response.render('archief2024.liquid', {stories: apiResponseJSON.data})
 })
 
 app.get('/archief-zonder-datum', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[district][_in]': 'algemeen, nieuw-west, oost, zuidoost',
     'filter[date][_null]': 'true',
@@ -270,40 +233,28 @@ app.get('/archief-zonder-datum', async function (request, response) {
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
-  // console.log(apiResponseJSON) // wat geeft dit terug?
+
    response.render('archiefzd.liquid', {stories: apiResponseJSON.data})
 })
 
-// Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
-// Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/collectie', async function (request, response) {
-  // console.log('POST ontvangen:', request.body)
 
-  // Stuur een POST request naar de messages database
-  // Een POST request bevat ook extra parameters, naast een URL
   const fetchRes = await fetch('https://fdnd-agency.directus.app/items/buurtcampuskrant_saved_stories', {
 
-    // overschrijf de standaard GET method
     method: 'POST',
 
-    // geef de body mee als JSON string
     body: JSON.stringify({
-      // Dit is zodat we ons artikel straks weer terug kunnen vinden met ons filter
       story: request.body.id,
       user: 1
     }),
 
-    // En vergeet deze HTTP headers niet: hiermee vertellen we de server dat we JSON doorsturen
-    // (In realistischere projecten zou je hier ook authentication headers of een sleutel meegeven)
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
     }
   })
 
   const fetchData = await fetchRes.json()
-  // console.log('Directus response:', fetchData)
 
-  // zonder redirect wordt in de browser niets geplaats, alleen in directus
   if (fetchData.errors) {
         response.redirect('/collectie?error=true')
     } else {
@@ -313,17 +264,15 @@ app.post('/collectie', async function (request, response) {
 })
 
 app.get('/collectie', async function (request, response) {
-  // Ophalen saved stories van user 1 (ik)
+
   const savedRes = await fetch('https://fdnd-agency.directus.app/items/buurtcampuskrant_saved_stories?filter[user][_eq]=1&fields=id,story')
   const savedData = await savedRes.json()
 
-  // Ophalen van de details van story
   const ids = savedData.data.map(item => item.story).join(',')
 
   const storiesRes = await fetch(`https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?filter[id][_in]=${ids}&fields=id,title,date,cover.id`)
   const storiesData = await storiesRes.json()
 
-  // Koppel het saved record ID aan elke story
     const stories = storiesData.data.map(story => {
         const saved = savedData.data.find(s => s.story === story.id)
         console.log('story.id:', story.id, 'saved:', saved)
@@ -348,32 +297,26 @@ app.post('/collectie/verwijder', async function (request, response) {
     response.redirect('/collectie')
 })
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
 
 app.get('/:slug', async function (request, response) {
    const slug = request.params.slug
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
+
    const params = {
     'filter[slug]': slug,
     'fields': 'title, intro, cover.id, body, target_group, slug'
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
-  // console.log(apiURL)
 
   const apiResponse = await fetch(apiURL)
   const apiResponseJSON = await apiResponse.json()
 
   const story = apiResponseJSON.data[0]
-  // console.log(personResponseJSON.data)
+
    response.render('detail.liquid', {story: story})
 })
 
-// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
