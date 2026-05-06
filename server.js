d38742fb5359c99848b9c5e2cfba94cc919fc97e
EmreBahar0912 <e.bahar906@gmail.com>
2026-05-06 11:15:07 +0200

    Archief files aangemaakt & route 2023 #7
    Alle liquid files aangemaakt voor het archief. En een route aangemaakt in server.js voor 2023.

--- server.js
+++ server.js
@@ -218,6 +218,25 @@ app.get('/algemeen-doelgroep-wijkpartner', async function (request, response) {
    response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
+app.get('/archief-2023', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   const params = {
+    'filter[district]': 'zuidoost',
+    'filter[date][_gte]': '2023-01-01',
+    'filter[date][_lte]': '2023-12-31',
+    'fields': 'title, intro, date, cover.id, slug'
+  }
+
+  const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
+  console.log(apiURL)
+
+  const apiResponse = await fetch(apiURL)
+  const apiResponseJSON = await apiResponse.json()
+  // console.log(personResponseJSON.data)
+   response.render('archief2023.liquid', {stories: apiResponseJSON.data})
+})
+
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/collectie', async function (request, response) {
